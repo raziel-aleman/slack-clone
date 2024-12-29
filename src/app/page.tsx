d546eb2b683cdba3f819bd543bc9274e5181153d
Raziel Aleman <razielali10@gmail.com>
2024-12-29 01:20:26 -0900

    convex db, authentication, workspace creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-import { AuthScreen } from "@/features/auth/components/auth-screen";
+"use client";
+
+import { UserButton } from "@/features/auth/components/user-button";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useEffect, useMemo } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-	return <AuthScreen />
+	const router = useRouter();
+	const [open, setOpen] = useCreateWorkspaceModal();
+
+	const { data, isLoading } = useGetWorkspaces();
+
+	const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+	useEffect(() => {
+		if (isLoading) return;
+
+		if (workspaceId) {
+			router.replace(`/workspace/${workspaceId}`);
+		} else if (!open) {
+			console.log("Open creation modal");
+			setOpen(true);
+		}
+	}, [workspaceId, isLoading, open, setOpen, router]);
+
+	return (
+		<>
+			<UserButton />
+		</>
+	);
 }
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -11,12 +11,42 @@ import { Separator } from "@/components/ui/separator";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { SignInFlow } from "../types";
+import React, { useState } from "react";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { TriangleAlert } from "lucide-react";
 
 interface SignInCardProps {
 	setState: (state: SignInFlow) => void;
 }
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
+	const { signIn } = useAuthActions();
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [pending, setPending] = useState(false);
+	const [error, setError] = useState("");
+
+	const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		setPending(true);
+		signIn("password", { email, password, flow: "signIn" })
+			.catch(() => {
+				setError("Invalid email or password");
+			})
+			.finally(() => {
+				setPending(false);
+			});
+	};
+
+	const onProviderSignIn = (value: "github" | "google") => {
+		setPending(true);
+		signIn(value).finally(() => {
+			setPending(false);
+		});
+	};
+
 	return (
 		<Card className="w-full h-full p-8">
 			<CardHeader className="px-0 pt-0">
@@ -25,20 +55,26 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 					Use your email or another service to continue
 				</CardDescription>
 			</CardHeader>
+			{!!error && (
+				<div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+					<TriangleAlert className="size-4" />
+					<p>{error}</p>
+				</div>
+			)}
 			<CardContent className="space-y-5 px-0 pb-0">
-				<form className="space-y-2.5">
+				<form onSubmit={onPasswordSignIn} className="space-y-2.5">
 					<Input
-						disabled={false}
-						value=""
-						onChange={() => {}}
+						disabled={pending}
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
 						placeholder="Email"
 						type="email"
 						required
 					/>
 					<Input
-						disabled={false}
-						value=""
-						onChange={() => {}}
+						disabled={pending}
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
 						placeholder="Password"
 						type="password"
 						required
@@ -47,7 +83,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 						type="submit"
 						className="w-full"
 						size="lg"
-						disabled={false}
+						disabled={pending}
 					>
 						Continue
 					</Button>
@@ -55,8 +91,8 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 				<Separator />
 				<div className="flex flex-col gap-y-2.5">
 					<Button
-						disabled={false}
-						onClick={() => {}}
+						disabled={pending}
+						onClick={() => onProviderSignIn("google")}
 						variant="outline"
 						size="lg"
 						className="w-full relative"
@@ -65,8 +101,8 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
 						Continue with Google
 					</Button>
 					<Button
-						disabled={false}
-						onClick={() => {}}
+						disabled={pending}
+						onClick={() => onProviderSignIn("github")}
 						variant="outline"
 						size="lg"
 						className="w-full relative"
