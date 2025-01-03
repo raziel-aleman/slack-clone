749646936854d66ad54c90c8e40cd39ead41bd77
Raziel Aleman <razielali10@gmail.com>
2025-01-03 00:16:20 -0900

    sidebar component, workspace switcher component hydration fix

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Toolbar from "./toolbar";
+import { Sidebar } from "./sidebar";
 
 interface WorkspaceIdLayoutProps {
 	children: React.ReactNode;
@@ -11,7 +12,10 @@ const WorkspaceLayout = ({ children }: WorkspaceIdLayoutProps) => {
 	return (
 		<div className="h-full">
 			<Toolbar />
-			{children}
+			<div className="flex h-[calc(100vh-40px)]">
+				<Sidebar />
+				{children}
+			</div>
 		</div>
 	);
 };
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,19 +1,7 @@
 "use client";
 
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
-// interface WorkspaceIdPageProps {
-// 	params: {
-// 		workspaceId: string;
-// 	};
-// }
-
 const WorkspaceIdPage = () => {
-	const workspaceId = useWorkspaceId();
-	const { data } = useGetWorkspace({ id: workspaceId });
-
-	return <div>Data: {JSON.stringify(data)}</div>;
+	return <div>Workspace Id page</div>;
 };
 
 export default WorkspaceIdPage;
