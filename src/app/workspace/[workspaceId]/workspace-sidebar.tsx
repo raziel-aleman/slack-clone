4093c2e6d2b500a75a312853d66ec712ecb8ea63
Raziel Aleman <razielali10@gmail.com>
2025-02-15 00:32:22 -0900

    create channel functionality

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -14,9 +14,12 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { WorkspaceSection } from "./workspace-section";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { UserItem } from "./user-item";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 
 export const WorkspaceSidebar = () => {
 	const workspaceId = useWorkspaceId();
+
+	const [_open, setOpen] = useCreateChannelModal();
 
 	const { data: member, isLoading: memberLoading } = useCurrentMember({
 		workspaceId,
@@ -69,7 +72,9 @@ export const WorkspaceSidebar = () => {
 			<WorkspaceSection
 				label="Channels"
 				hint="New channel"
-				onNew={() => {}}
+				onNew={
+					member.role === "admin" ? () => setOpen(true) : undefined
+				}
 			>
 				{channels?.map((item) => (
 					<SidebarItem
---
+++ src/features/channels/components/create-channel-modal.tsx
@@ -0,0 +1,66 @@
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+
+import { useCreateChannelModal } from "../store/use-create-channel-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { useCreateChannel } from "../api/use-create-channel";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+export const CreateChannelModal = () => {
+	const workspaceId = useWorkspaceId();
+	const [open, setOpen] = useCreateChannelModal();
+	const { mutate, isPending } = useCreateChannel();
+	const [name, setName] = useState("");
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+		setName(value);
+	};
+	const handleClose = () => {
+		setName("");
+		setOpen(false);
+	};
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault;
+		mutate(
+			{ name, workspaceId },
+			{
+				onSuccess: (id) => {
+					// TODO: Redirect to new channel
+					handleClose();
+				},
+			}
+		);
+	};
+
+	return (
+		<Dialog open={open} onOpenChange={handleClose}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Add a channel</DialogTitle>
+				</DialogHeader>
+				<form onSubmit={handleSubmit} className="space-y-4">
+					<Input
+						value={name}
+						disabled={isPending}
+						onChange={handleChange}
+						required
+						autoFocus
+						minLength={3}
+						maxLength={80}
+						placeholder="e.g. plan-budget"
+					/>
+				</form>
+				<div className="flex justify-end">
+					<Button disabled={false}>Create</Button>
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+};
